805d71088ce3d63ab6a8a6acd6f3f4919a904dd4
moon <anstnqls554@gmail.com>
2022-07-06 00:45:02 +0900

    interview 섹션 완성

--- js/script.js
+++ js/script.js
@@ -11,6 +11,23 @@ $(document).ready(function () {
             },
             navigation:{
                 nextEl:'.sw-visual-next'
+            },
+            on:{
+                activeIndexChange: function () {
+                    const videos = document.querySelectorAll(".swiper-slide video");
+                    videos[this.activeIndex].currentTime = 0;
+                    videos[this.activeIndex].play();
+                    console.log(videos[this.activeIndex].duration)
+                    // $('.video').eq(this.realIndex).get(0).play();
+                }
+            }
+            // 전체길이 가져오고 구글 검색 " 영상 현재 재생 위치 "를 구해서 이 재생시간이 총길이에 부합하면 if를 사용해서 영상에 있는 버튼을 클릭하게
+        });
+        let swiper2 = new Swiper('.sw-interview',{
+            loop:true,
+            navigation:{
+                prevEl:'.sw-iv-prev',
+                nextEl:'.sw-iv-next'
             }
         });
     };
