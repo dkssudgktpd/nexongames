4b142d6988be8c973b2929786321212878449e2d
moon <anstnqls554@gmail.com>
2022-07-14 00:01:07 +0900

    html css js 수정중

--- js/script.js
+++ js/script.js
@@ -1,41 +1,51 @@
 $(document).ready(function () {
     window.onload = function () {
+        
+        let header = $('.header');
+        let mainMenu = $('.mainmenu');
+        let mainMenuMaxHeight = mainMenu.outerHeight();
+        let mainMenuMinHeight = header.outerHeight();
+        let videoplay = $('.video');
+
+        mainMenu.mouseenter(function () {
+            header.css('height', mainMenuMaxHeight);
+        });
+
+        mainMenu.mouseleave(function () {
+            header.css('height', mainMenuMinHeight);
+        });
+
         let swiper = new Swiper(".sw-visual", {
-            loop:true,
-            autoplay:{
-            delay:20000
+            loop: true,
+            autoplay: {
+                delay: 20000
             },
-            pagination:{
+            pagination: {
                 el: ".sw-visual-pg",
                 type: "fraction"
             },
-            navigation:{
-                nextEl:'.sw-visual-next'
-            },
-            on:{
-                activeIndexChange: function () {
-                    const videos = document.querySelectorAll(".swiper-slide video");
-                    videos[this.activeIndex].currentTime = 0;
-                    videos[this.activeIndex].play();
-                    nextVideo(videos[this.activeIndex].duration*1000)
-                    
-                    // $('.video').eq(this.realIndex).get(0).play();
+            navigation: {
+                nextEl: '.sw-visual-next'
+            },on:{
+                slideChange : function(){
+                    let videoplay = $('.swiper-slide').find('video');
+                    videoplay.removeAttr('autoplay','autoplay');
+                    videoplay.attr('autoplay','autoplay');
+                    // $.each(videoplay,function(){
+                    // });
                 }
             }
-            // 전체길이 가져오고 구글 검색 " 영상 현재 재생 위치 "를 구해서 이 재생시간이 총길이에 부합하면 if를 사용해서 영상에 있는 버튼을 클릭하게
+
         });
-        let swiper2 = new Swiper('.sw-interview',{
-            loop:true,
-            navigation:{
-                prevEl:'.sw-iv-prev1',
-                nextEl:'.sw-iv-next1'
+
+        let swiper2 = new Swiper('.sw-interview', {
+            loop: true,
+            navigation: {
+                prevEl: '.sw-iv-prev1',
+                nextEl: '.sw-iv-next1'
             }
         });
     };
 });
 
-function nextVideo(time){
-    setTimeout(() => {
-        document.querySelector(".sw-visual-next").click();
-    },time)
-}
+
